d42bb282fe2617e7033e7eb76922c1b501f11ba9
Lukas <lukasduechting@mailbox.org>
2022-07-11 10:23:19 +0200

    a lot of config

--- backend/src/main/java/com/github/mysterix5/capstone/CapstoneApplication.java
+++ backend/src/main/java/com/github/mysterix5/capstone/CapstoneApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.github.mysterix5.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CapstoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CapstoneApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/github/mysterix5/capstone/CapstoneApplicationTests.java
+++ backend/src/test/java/com/github/mysterix5/capstone/CapstoneApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.github.mysterix5.capstone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CapstoneApplicationTests {
 
 	@Test
 	void contextLoads() {
