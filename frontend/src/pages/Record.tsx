7aabdc4d4eab65817b7eb5d57c04d88c44a986d3
Lukas <lukasduechting@mailbox.org>
2022-07-17 09:29:08 +0200

    add a Record page

---
+++ frontend/src/pages/Record.tsx
@@ -0,0 +1,14 @@
+import {Box, Typography} from "@mui/material";
+
+
+export default function Record(){
+
+
+    return (
+        <Box>
+            <Typography variant={"h4"}>
+                Record page
+            </Typography>
+        </Box>
+    )
+}
