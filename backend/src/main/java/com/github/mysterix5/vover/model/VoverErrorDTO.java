483b8c139e316a1752ba84c55cf2df12235da94b
Lukas <lukasduechting@mailbox.org>
2022-07-22 16:04:06 +0200

    bugfix with maven dependency

--- backend/src/main/java/com/github/mysterix5/vover/model/VoverErrorDTO.java
+++ backend/src/main/java/com/github/mysterix5/vover/model/VoverErrorDTO.java
@@ -1,7 +1,6 @@
 package com.github.mysterix5.vover.model;
 
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,12 +11,12 @@ public class VoverErrorDTO {
     private String message;
     private List<String> subMessages = new ArrayList<>();
 
-    public VoverErrorDTO(@NotNull MultipleSubErrorException e){
+    public VoverErrorDTO(MultipleSubErrorException e){
         message = e.getMessage();
         subMessages = e.getSubMessages();
     }
 
-    public VoverErrorDTO(@NotNull Exception e){
+    public VoverErrorDTO(Exception e){
         message = e.getMessage();
     }
 
