2e51247351c5a366411008a21c1307d643ab7add
Lukas <lukasduechting@mailbox.org>
2022-08-06 14:35:46 +0200

    - show scope choice on primary page - save scope in backend if changed

--- frontend/src/pages/primary/Scope.tsx
+++ frontend/src/pages/primary/Scope.tsx
@@ -1,5 +1,14 @@
-import {Box, Checkbox, FormControl, FormControlLabel, FormGroup, Typography} from "@mui/material";
-import {ChangeEvent} from "react";
+import {
+    Box,
+    Button,
+    Checkbox,
+    ClickAwayListener,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    Popper
+} from "@mui/material";
+import {ChangeEvent, useState, MouseEvent} from "react";
 
 interface ScopeProps {
     friends: string[],
@@ -8,32 +17,53 @@ interface ScopeProps {
 }
 
 export default function Scope(props: ScopeProps) {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const handleClick = (event: MouseEvent<HTMLElement>) => {
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'scope-popper' : undefined;
+
+    const handleClickAway = () => {
+        setAnchorEl(null);
+    };
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         console.log(`event name: ${event.target.name}, event checked: ${event.target.checked}`);
         console.log(props.scope);
 
-        if(event.target.checked){
+        if (event.target.checked) {
             props.setScope([...props.scope, event.target.name]);
-        }else{
-            props.setScope(props.scope.filter(s => s!==event.target.name));
+        } else {
+            props.setScope(props.scope.filter(s => s !== event.target.name));
         }
     };
 
     return (
-        <Box>
-            <Typography>Scope choice</Typography>
-            <FormControl>
-                <FormGroup>
-                    {
-                        props.friends.map(f =>
-                            <FormControlLabel key={f} control={
-                                <Checkbox checked={props.scope.includes(f)} name={f} onChange={handleChange}/>
-                            } label={f}/>
-                        )
-                    }
-                </FormGroup>
-            </FormControl>
-        </Box>
-    )
+        <ClickAwayListener onClickAway={handleClickAway}>
+            <div>
+                <Button variant={"contained"} size={"small"} aria-describedby={id} type="button" onClick={handleClick}>
+                    scope choice
+                </Button>
+                <Popper id={id} open={open} anchorEl={anchorEl}>
+                    <Box border={1} padding={1} bgcolor={"black"}>
+                        <FormControl>
+                            <FormGroup>
+                                {
+                                    props.friends.map(f =>
+                                        <FormControlLabel key={f} control={
+                                            <Checkbox checked={props.scope.includes(f)} name={f}
+                                                      onChange={handleChange}/>
+                                        } label={f}/>
+                                    )
+                                }
+                            </FormGroup>
+                        </FormControl>
+                    </Box>
+                </Popper>
+            </div>
+        </ClickAwayListener>
+    );
 }
